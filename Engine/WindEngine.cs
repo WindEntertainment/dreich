935fe7b1e4b9a7e4fce8b41e0d64ad0c23d656b5
NeutrinoZh <neutrinozh@gmail.com>
2024-09-12 20:08:35 +0300

    error handling

--- Engine/WindEngine.cs
+++ Engine/WindEngine.cs
@@ -5,58 +5,53 @@ using Wind.Mathf;
 
 namespace Wind
 {
-    public class WindEngine
+  public class WindEngine
+  {
+    private Logger _logger;
+    private Window _window;
+
+    public WindEngine()
     {
-        private Logger _logger;
-        private Window _window;
+      WindServices.Instance.Register(new LoggerManager());
+      _logger = new Logger("WindEngine", WindServices.Instance.Get<LoggerManager>());
 
-        public WindEngine()
-        {
-            WindServices.Instance.Register(new LoggerManager());
-            _logger = new Logger("WindEngine", WindServices.Instance.Get<LoggerManager>());
+      _logger.Info("Launch engine initialization process");
 
-            _logger.Info("Launch engine initialization process");
+      if (SDL_Init(SDL_INIT_VIDEO) < 0)
+        _logger.Error($"SDL could not initialize! SDL_Error: {SDL_GetError()}");
 
-            if (SDL_Init(SDL_INIT_VIDEO) < 0)
-                _logger.Error($"SDL could not initialize! SDL_Error: {SDL_GetError()}");
+      _window = new Window(
+          "Hello World!",
+          new Vector2i(800, 600)
+      );
 
-            try
-            {
-                _window = new Window(
-                    "Hello World!",
-                    new Vector2i(800, 600)
-                );
-                WindServices.Instance.Register(_window);
-            }
-            catch (Exception ex)
-            {
-                _logger.Error(ex.Message);
-            }
-        }
+      WindServices.Instance.Register(_window);
 
-        public void Loop()
-        {
-            _logger.Info("Launch engine loop");
-
-            bool quit = false;
-
-            while (!quit)
-            {
-                while (SDL_PollEvent(out SDL_Event e) != 0)
-                {
-                    if (e.type == SDL_EventType.SDL_QUIT)
-                        quit = true;
-                }
-
-                // OpenGL.Gl.ClearColor(0, 0, 0, 0);
-            }
-
-            Dispose();
-        }
-
-        private void Dispose()
-        {
-            SDL_Quit();
-        }
     }
+
+    public void Loop()
+    {
+      _logger.Info("Launch engine loop");
+
+      bool quit = false;
+
+      while (!quit)
+      {
+        while (SDL_PollEvent(out SDL_Event e) != 0)
+        {
+          if (e.type == SDL_EventType.SDL_QUIT)
+            quit = true;
+        }
+
+        // OpenGL.Gl.ClearColor(0, 0, 0, 0);
+      }
+
+      Dispose();
+    }
+
+    private void Dispose()
+    {
+      SDL_Quit();
+    }
+  }
 }
