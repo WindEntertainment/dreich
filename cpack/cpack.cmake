55b73d97265feadfea837d7c5b253df92b1c51ef
Angoliuk <gamongara@gmail.com>
2024-09-15 21:39:29 +0300

    test

--- cpack/cpack.cmake
+++ cpack/cpack.cmake
@@ -1,12 +1,12 @@
 set(CPACK_GENERATOR "ZIP")
-set(CPACK_PACKAGE_FILE_NAME "wind-core")
+set(CPACK_PACKAGE_FILE_NAME "Logger")
 
 set(CPACK_SOURCE_IGNORE_FILES
   "/build/;/.git/;/.vscode/;/CMakeFiles/;/CMakeCache.txt;/Makefile;/cmake_install.cmake"
 )
 
 
-install(DIRECTORY "${CMAKE_SOURCE_DIR}/wind/core/" DESTINATION "./core")
+install(DIRECTORY "${CMAKE_SOURCE_DIR}/Logger/" DESTINATION "./Logger")
 install(FILES ${CMAKE_SOURCE_DIR}/conanfile.py DESTINATION .)
 install(FILES ${CMAKE_SOURCE_DIR}/CMakeLists.txt DESTINATION .)
 install(FILES ${CMAKE_SOURCE_DIR}/.env DESTINATION .)
