8fb98280d79c7a912e945b93244d48847b0cb44c
NeutrinoZh <neutrinozh@gmail.com>
2024-09-29 11:28:06 +0300

    post init renderer

--- game/Src/Game.cs
+++ game/Src/Game.cs
@@ -8,11 +8,17 @@ internal class Game {
   [DllImport("wind.so", EntryPoint = "windCreateWindow", CallingConvention = CallingConvention.Cdecl)]
   public static extern IntPtr WindCreateWindow(int position_x, int position_y, int width, int height, string title);
 
+  [DllImport("wind.so", EntryPoint = "windPostInitRenderer", CallingConvention = CallingConvention.Cdecl)]
+  public static extern bool WindPostInitRenderer();
+
   public static void Main(string[] args) {
-    var result = WindInitRenderer();
-    Console.WriteLine(result);
+    if (!WindInitRenderer())
+      return;
 
     WindCreateWindow(100, 100, 800, 600, "Hello From C#");
+
+    if (!WindPostInitRenderer())
+      return;
 
     Task.Delay(1000).Wait();
     // WindEngine engine = new();
