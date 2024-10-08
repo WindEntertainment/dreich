42c0453969bcea192cfe40dc29e425b10596d850
Angoliuk <gamongara@gmail.com>
2024-10-08 13:35:07 +0300

    wasm exe test

--- cmake/executable.cmake
+++ cmake/executable.cmake
@@ -1,3 +1,2 @@
-add_executable(${PROJECT_NAME}-engine ${HEADERS} ${SOURCES} ./native/src/main.cpp)
-target_include_directories(${PROJECT_NAME}-engine PRIVATE ./native/include/)
-target_link_libraries(${PROJECT_NAME}-engine PUBLIC ${PROJECT_NAME})
+add_executable(${PROJECT_NAME} ${HEADERS} ${SOURCES} ./native/src/main.cpp)
+target_include_directories(${PROJECT_NAME} PRIVATE ./native/include/)
